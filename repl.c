ae2c99fbf5f5de918f3ff91cd1b7266d73c9fa86
Zachary T Jones <zt.jones88@gmail.com>
2018-12-29 12:21:43 -0500

    Add readline support

--- repl.c
+++ repl.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
-const int BUFFER_SIZE = 2048;
-static char input_buffer[BUFFER_SIZE];
+#ifdef _WIN32
+#include <string.h>
+
+static int buffer_size = 2048;
+static char buffer[buffer_size];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, buffer_size, stdin);
+  char* cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+void add_history(char* unused) {}
+#else
+#include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
   puts("Lispy Version 0.0.1");
   puts("Press Ctrl-c to exit.\n");
 
   while(true) {
-    fputs("lispy> ", stdout);
+    char* input = readline("lispy> ");
 
-    fgets(input_buffer, BUFFER_SIZE, stdin);
+    /* Add user input to history, allowing for arrow navigation */
+    add_history(input);
+    printf("No you're a %s\n", input);
 
-    printf("No you're a %s", input_buffer);
+    /* free retrieved input */
+    free(input);
   }
 
   return 0;
